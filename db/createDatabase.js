01d3347d03cc005563306de84c57f2012fc52487
Vern_Pherus <145563323+VernPherus@users.noreply.github.com>
2024-10-18 07:24:21 +0800

    added sql dump file idk

--- db/createDatabase.js
+++ db/createDatabase.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql2");
-const fs = requrie("fs");
+const fs = require("fs");
 const path = require("path");
 
 exports.createDatabase = async () => {
@@ -8,10 +8,12 @@ exports.createDatabase = async () => {
       host: "localhost",
       user: "root",
       password: "",
+      database: "upang_eats",
+      multipleStatements: true,
     });
 
     const dumpFilePath = path.join(__dirname, "upang_eats.sql");
-    const sqlDump = fs.reafFileSync(dumpFilePath, "utf8");
+    const sqlDump = fs.readFileSync(dumpFilePath, "utf8");
 
     await connection.promise().query(sqlDump);
     console.log("Database and tables created successfully!");
